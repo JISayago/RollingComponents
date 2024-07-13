0a14bf1d4b6a2bdde3ca7876b59d039188138e66
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-07-13 17:02:29 -0300

    se corrige ruteo de la hero page desde index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 if ((!sessionStorage.getItem('vioHeroPage')) || (sessionStorage.getItem('vioHeroPage')==='false')) {
     sessionStorage.setItem('vioHeroPage', 'false');
-    window.location.href = "./Paginas/heroPage.html"
+    window.location.href = "./pages/heroPage.html"
 }
