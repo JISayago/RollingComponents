e413d5a513642e883c734ab2785957f5c2c62695
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-07-17 20:00:57 -0300

    se agrego la limitacion de acceso para admin solamente

--- js/gestionUsuario.js
+++ js/gestionUsuario.js
@@ -1,15 +1,24 @@
+const userLogeado = JSON.parse(sessionStorage.getItem('usuarioLogeado'));
+const body = document.getElementById('body-usuarios');
+body.classList.add('d-none');
+if (!userLogeado ||userLogeado.tipoUsuario !== 0 ) {
+    alert('¡Permisos insuficientes!');
+    window.location.href = "../pages/login.html"
+}
+
+body.classList.remove('d-none');
 const tabla = document.getElementById('cuerpo-tabla');
 const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
 
 /*let nuevoUsuario = {
-    nombre: "Bloqueado",
-    apellido: "bloqueado",
-    username: "bloq",
-    email: "bloq@gmail.com",
-    password: "123456789",
-    tipoUsuario: 1,
-    esteBloqueado: true
+nombre: "Bloqueado",
+apellido: "bloqueado",
+username: "bloq",
+email: "bloq@gmail.com",
+password: "123456789",
+tipoUsuario: 1,
+esteBloqueado: true
 };
 usuarios.push(nuevoUsuario);
 localStorage.setItem('usuarios', JSON.stringify(usuarios));
@@ -19,11 +28,8 @@ const adminNoAdmin = (username) => {
     
     if (usuarioLS) {
         usuarioLS.tipoUsuario = usuarioLS.tipoUsuario === 0 ? 1 : 0;
-
         const index = usuarios.findIndex(u => u.username === username);
-
         usuarios[index] = usuarioLS;
-
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
     }
     window.location.href = "../pages/gestionUsuarios.html"
@@ -33,13 +39,12 @@ const bloquearDesbloquear = (username) => {
     
     if (usuarioLS) {
         usuarioLS.estaBloqueado = !usuarioLS.estaBloqueado; 
-       console.log(usuarioLS)
+        console.log(usuarioLS)
         const index = usuarios.findIndex(u => u.username === username);
         usuarios[index] = usuarioLS;
-
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
     }
-
+    
     window.location.href = "../pages/gestionUsuarios.html";
 }
 tabla.innerHTML = usuarios.map(u => 
